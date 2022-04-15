31d62d52a46814ae7d5fa7d52144ca68d457ee64
baltabaevich <101479779+baltabaevich@users.noreply.github.com>
2022-04-16 05:32:01 +0600

    Add files via upload

---
+++ precache-manifest.37d2c9ec5d5f5d2f01385305670c70f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9019d37ba35e529b7a40",
+    "url": "/static/css/main.c6a543b6.chunk.css"
+  },
+  {
+    "revision": "9019d37ba35e529b7a40",
+    "url": "/static/js/main.9019d37b.chunk.js"
+  },
+  {
+    "revision": "627001a980ec78441dd6",
+    "url": "/static/js/1.627001a9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ea490a5c3313155988f1b6bf2009b000",
+    "url": "/index.html"
+  }
+];
